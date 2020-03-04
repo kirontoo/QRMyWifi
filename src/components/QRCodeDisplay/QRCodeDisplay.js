d7fc3b810b01c211a09e8ffae76488da211a35c9
Amy Dang <kirontoo@gmail.com>
2020-03-04 14:20:58 -0800

    add styling to export buttons

--- src/components/QRCodeDisplay/QRCodeDisplay.js
+++ src/components/QRCodeDisplay/QRCodeDisplay.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './QRCodeDisplay.css';
 
-const QRCodeDisplay = ( { title } ) => {
+const QRCodeDisplay = ( { title, exportToPdf, exportToImage } ) => {
 	return (
 		<div className='panel'>
-			<h1>{ title }</h1>
+			<h3>{ title }</h3>
 			<div id='canvas'></div>
+			<main>
+				<p className='b'>Export to: </p>
+				<div className='buttonContainer'>
+					<button className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib' onClick={ exportToPdf }>PDF</button>
+					<button className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib' onClick={ exportToImage }>Image</button>
+				</div>
+			</main>
 		</div>
 	);
 }
