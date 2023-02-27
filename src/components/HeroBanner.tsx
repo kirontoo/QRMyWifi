bd61c54e2aee497c8413c4fdeaab216c1bd1e1aa
Amy Dang <kirontoo@gmail.com>
2023-02-27 15:58:05 -0800

    ui: adjust for mobile devices

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -1,12 +1,17 @@
 function HeroBanner() {
   return (
-    <section className="py-12 sm:py-24 ">
-      <div className="w-11/12 sm:w-2/3 gap-8 lg:flex justify-center items-center lg:items-start flex-col  mb-5 sm:mb-10">
-        <h1 className="text-secondary text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-gray-800 font-black leading-7 md:leading-10 text-bold">
+    <section className="py-12 sm:py-24">
+      <div className="w-full lg:w-11/12 sm:w-2/3 gap-6 lg:gap-8 flex justify-center items-center lg:items-start flex-col mb-5 sm:mb-10 px-4 lg:px-0">
+        <h1 className="text-secondary text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-black leading-7 md:leading-10 text-bold text-center lg:text-left">
           Connect to WiFi in a snap with our QR code generator
         </h1>
-        <a className="btn btn-primary text-2xl" href="#qr-generator">Get Started</a>
-        <p className="lg:w-10/12 text-black() font-normal text-sm sm:text-lg">
+        <a
+          className="btn btn-primary text-lg sm:text-xl() lg:text-2xl"
+          href="#qr-generator"
+        >
+          Get Started
+        </a>
+        <p className="lg:w-10/12 text-black() font-normal text-md sm:text-lg text-center lg:text-left">
           Simplify your life with our QR code generator. No more complicated
           passwords, just scan and go!
         </p>
