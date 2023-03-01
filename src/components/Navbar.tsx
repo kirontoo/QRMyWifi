baf7834418f830e1b9a348259c285e2fece1f153
Amy Dang <kirontoo@gmail.com>
2023-03-01 15:59:31 -0800

    ui: add styling for card and navlinks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,16 @@
 function Navbar() {
   return (
-    <nav className="bg-secondary text-white flex flex-col md:flex-row gap-2 text-xl justify-center md:justify-between w-screen min-h-[6rem] items-center">
+    <nav className="bg-secondary text-white flex flex-col md:flex-row gap-2 text-xl justify-center md:justify-start min-h-[6rem] items-center">
       <div className="w-full md:w-[20rem] h-[6rem] bg-primary text-4xl flex items-center justify-center md:justify-start md:pl-20">
         IMAGE QRMyWifi
       </div>
-      <a className="md:pr-20 btn btn-secondary" href="#">
-      QR Generator
+
+      <a className="nav-link" href="#how-it-works">
+        How It Works
+      </a>
+
+      <a className="nav-link" href="#qr-generator">
+        QR Generator
       </a>
     </nav>
   );
