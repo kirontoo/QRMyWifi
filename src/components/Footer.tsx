9edcd5593f239ab9618fb6b6ac9aac6e590355ed
Amy Dang <kirontoo@gmail.com>
2023-03-02 13:37:26 -0800

    fix: blank target security risk

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ function Footer() {
     <div className="w-full bg-secondary text-white py-4 text-center ">
       <div className="w-9/12 mx-auto flex flex-col lg:flex-row justify-between items-center gap-4">
         <small className="text-center text-lg">&#169;2022 QRMyWifi</small>
-        <a href="https://ko-fi.com/F1F2J3RV9" target="_blank">
+        <a href="https://ko-fi.com/F1F2J3RV9" target="_blank" rel="noreferrer">
           <img
             height="36"
             style={{ border: "0px", height: "36px" }}
